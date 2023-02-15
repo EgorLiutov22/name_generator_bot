fb269f1928e32cf09bb5e22656caa57631a5bf9e
Олег <122548219+Oleold@users.noreply.github.com>
2023-02-15 22:41:21 +0300

    Update Telegram-Bot.py

--- Telegram-Bot.py
+++ Telegram-Bot.py
@@ -1,21 +1,21 @@
-#import
-import telebot
-import BasicGenerator
+def main():
+	import telebot
+	import BasicGenerator
 
-bot = telebot.TeleBot("token")
+	bot = telebot.TeleBot("token")
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Это генератор странных имен, для продолжения напишите имя и уровень странности")
+	@bot.message_handler(commands=['start', 'help'])
+	def send_welcome(message):
+		bot.reply_to(message, "Это генератор странных имен, для продолжения напишите имя и уровень странности")
 
-@bot.message_handler(func=lambda message: True)
-def Name_print(message):
-	mm = (message.text).split()
-	if len(mm)==2 and mm[1].isdigit():
-		try:
-			b = BasicGenerator.BasicGenerator(int(mm[1]), mm[0])
-			bot.send_message(message.chat.id, b.generate_nick())
-		except:
-			bot.send_message(message.chat.id, "Ошибка")
+	@bot.message_handler(func=lambda message: True)
+	def Name_print(message):
+		mm = (message.text).split()
+		if len(mm)==2 and mm[1].isdigit():
+			try:
+				b = BasicGenerator.BasicGenerator(int(mm[1]), mm[0])
+				bot.send_message(message.chat.id, b.generate_nick())
+			except:
+				bot.send_message(message.chat.id, "Ошибка")
 
-bot.infinity_polling()
+	bot.infinity_polling()
