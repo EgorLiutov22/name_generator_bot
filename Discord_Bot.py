9e128c46206a668ba96a8f0486b15344a42fa984
Олег <122548219+Oleold@users.noreply.github.com>
2023-02-15 22:40:32 +0300

    Update Discord_Bot.py

--- Discord_Bot.py
+++ Discord_Bot.py
@@ -1,26 +1,27 @@
-import discord
-import BasicGenerator
+def main():
+    import discord
+    import BasicGenerator
 
-intents = discord.Intents.default()
-intents.message_content = True
+    intents = discord.Intents.default()
+    intents.message_content = True
 
-client = discord.Client(intents=intents)
+    client = discord.Client(intents=intents)
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
+    @client.event
+    async def on_ready():
+        print(f'We have logged in as {client.user}')
 
-@client.event
-async def on_message(message):
-    mm = (message.content).split()
-    if message.author == client.user:
-        return
-    if len(mm) == 2 and mm[1].isdigit():
-        try:
-            b = BasicGenerator.BasicGenerator(int(mm[1]), mm[0])
-            await message.channel.send(b.generate_nick())
-        except:
-            await message.channel.send("Ошибка")
-    else:
-        await message.channel.send("Введите имя и уровень странности")
-client.run('token')
+    @client.event
+    async def on_message(message):
+        mm = (message.content).split()
+        if message.author == client.user:
+            return
+        if len(mm) == 2 and mm[1].isdigit():
+            try:
+                b = BasicGenerator.BasicGenerator(int(mm[1]), mm[0])
+                await message.channel.send(b.generate_nick())
+            except:
+                await message.channel.send("Ошибка")
+        else:
+            await message.channel.send("Введите имя и уровень странности")
+    client.run('token')
